3022256baa5f3a1e6baabd5894fea8fa2977544c
Adam McDaniel <adam@jolicloud.org>
2011-07-22 15:26:18 -0600

    fg1406: Added XMLHttpRequest() query structure

--- demos/offline/network-detector.js
+++ demos/offline/network-detector.js
@@ -1,10 +1,22 @@
 function testXHR(callback) {
+  var xhr = new XMLHttpRequest();
+  var noResponseTimer = setTimeout(function() {
+    xhr.abort();
+  }, 5000);
+
+  xhr.onreadystatechange = function(event) {
+    if (xhr.readyState != 4)
+      return;
+
+    clearTimeout(noResponseTimer);
+  }
 }
 
 function fireNetworkEvent(state) {
 }
 
 function testNetwork() {
+  testXHR(fireNetworkEvent);
 }
 
 function lostNetwork() {

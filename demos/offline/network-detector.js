52616ee3f309c13a8bcc2017eeb68af186f0e61b
Adam McDaniel <adam@jolicloud.org>
2011-07-22 14:09:15 -0600

    fg1405: Started the network status detector, creating function structure

---
+++ demos/offline/network-detector.js
@@ -0,0 +1,14 @@
+function testXHR(callback) {
+}
+
+function fireNetworkEvent(state) {
+}
+
+function testNetwork() {
+}
+
+function lostNetwork() {
+}
+
+window.addEventListener('online', testNetwork, false);
+window.addEventListener('offline', lostNetwork, false);

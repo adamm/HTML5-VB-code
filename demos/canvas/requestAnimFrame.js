39d3f24740e7ab0bce05ad920d40ac5718d33404
Adam McDaniel <adam@jolicloud.org>
2011-07-25 03:05:16 -0600

    Apply requestTimeout() and clearRequestTimeout() from Joe Lambert's blog.

--- demos/canvas/requestAnimFrame.js
+++ demos/canvas/requestAnimFrame.js
@@ -9,3 +9,39 @@ window.requestAnimFrame = (function(){
             window.setTimeout(callback, 1000 / 60);
           };
 })();
+
+window.requestTimeout = function(fn, delay) {
+    if( !window.requestAnimationFrame       && 
+        !window.webkitRequestAnimationFrame && 
+        !window.mozRequestAnimationFrame    && 
+        !window.oRequestAnimationFrame      && 
+        !window.msRequestAnimationFrame)
+            return window.setTimeout(fn, delay);
+
+    var start = new Date().getTime(),
+        handle = new Object();
+
+    function loop(){
+        var current = new Date().getTime(),
+        delta = current - start;
+
+        delta >= delay ? fn.call() : handle.value = requestAnimFrame(loop);
+    };
+
+    handle.value = requestAnimFrame(loop);
+    return handle;
+};
+
+window.clearRequestTimeout = function(handle) {
+    window.cancelAnimationFrame ? 
+      window.cancelAnimationFrame(handle.value) :
+    window.webkitCancelRequestAnimationFrame ?
+      window.webkitCancelRequestAnimationFrame(handle.value)   :
+    window.mozCancelRequestAnimationFrame ?
+      window.mozCancelRequestAnimationFrame(handle.value) :
+    window.oCancelRequestAnimationFrame ?
+      window.oCancelRequestAnimationFrame(handle.value) :
+    window.msCancelRequestAnimationFrame ?
+      msCancelRequestAnimationFrame(handle.value) :
+    clearTimeout(handle);
+};

293b6bf3f17b70222f1a8ff2ee1387584b2c8ebd
Adam McDaniel <adam@jolicloud.org>
2011-07-25 03:03:48 -0600

    fg1061: Use Opera and Microsoft versions of requestAnimFrame in the shim, maybe one day

--- demos/canvas/requestAnimFrame.js
+++ demos/canvas/requestAnimFrame.js
@@ -3,4 +3,6 @@ window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame       || 
           window.webkitRequestAnimationFrame || 
           window.mozRequestAnimationFrame    || 
+          window.oRequestAnimationFrame      || 
+          window.msRequestAnimationFrame     || 
 })();
